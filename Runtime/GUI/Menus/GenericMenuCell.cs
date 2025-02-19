0f3ee73ba9407c457c1be872ea598da0638833b5
Marc313 <77342337+Marc313@users.noreply.github.com>
2025-02-19 13:49:40 +0100

    update: Imported Version 1.3.1

--- Runtime/GUI/Menus/GenericMenuCell.cs
+++ Runtime/GUI/Menus/GenericMenuCell.cs
@@ -8,7 +8,7 @@ namespace MarcoUtilities.GUI.Menus
     /// <typeparam name="T"></typeparam>
     public abstract class GenericMenuCell<T> : MonoBehaviour where T : MenuData
     {
-        protected T MenuData { get; private set; }
+        protected T MenuData { get; protected set; }
 
         public void SetData(T data)
         {
